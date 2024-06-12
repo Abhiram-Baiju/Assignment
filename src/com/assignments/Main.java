46c36e668b612e63bd4d2289da19c04917fb4c7b
abhiram <abhiram@seemymachines.com>
2024-06-12 16:14:51 +0530

    Edited the code

--- src/com/assignments/Main.java
+++ src/com/assignments/Main.java
@@ -1,8 +1,23 @@
 package com.assignments;
 
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        Scanner n= new Scanner(System.in);
+        int condition;
+        System.out.println("Odd or Even");
+        do{
+            System.out.println("Enter the number ");
+            int num=n.nextInt();
+            if(num%2==0){
+                System.out.println(num+" is even");
+            }
+            else{
+                System.out.println(num+" is odd");
+            }
+            System.out.println("do you want to continue\n1-yes\n2-no");
+            condition=n.nextInt();
+        }while (condition==1);
+        System.out.println("program terminated");
     }
 }

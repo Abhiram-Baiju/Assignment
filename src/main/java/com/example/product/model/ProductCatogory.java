e06708b844e6502b4176916b3038bdb632f4d280
Abhiram <abhiram@seemymachines.com>
2024-06-27 08:57:48 +0530

    New task

---
+++ src/main/java/com/example/product/model/ProductCatogory.java
@@ -0,0 +1,13 @@
+package com.example.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductCatogory {
+    private String Pid;
+    private String Pname;
+}
